ab078fc2a5072f99a5196814b18c00c95b8f8c01
ATeaDaze <89423150+ATeaDaze@users.noreply.github.com>
2023-11-04 23:11:05 -0500

    Emoji Table: added copy-to-clipboard
    Added copy-to-clipboard with notification

--- emoji_table/scripts/main.js
+++ emoji_table/scripts/main.js
@@ -1,33 +1,17 @@
 // TODO: refactor without reversing order of list, remove duplicates, specific ranges, 
 // add CSS for HTML formatting (deprecated)
-let emojiCodeList = [ [128512, 128592], // Smileys 1
-                      [129296, 129304], // Smileys 2
-                      [129312, 129343], // Smileys 3
-                      [128100, 128132], // People 1
-                      [129304, 129311], // Gestures 1
-                      [128064, 128100], // Gestures 2 and Fashion
-                      [128000, 128064], // Animals 1
-                      [129408, 129426], // Animals 2
-                      [127789, 127872], // Food 1
-                      [129344, 129350], // Food 2
-                      [129351, 129375], // Food 3
-                      [129375, 129392], // Food 4
-                      [129472, 129473], // Cheese!\
-                      [127872, 127956], // Hobbies
-                      [128176, 128281], // Business and Media
-                      [127956, 127985], // Places 1
-                      [128331, 128335], // Religious buildings
-                      [128640, 128680], // Travel and Transport
-//                      [128680, 128681], // Police Siren
-                      [127744, 127789], // Weather
-                      [128293, 128306], // Symbols 1
-                      [128132, 128176], // Symbols 2
-                      [9904,   10161 ], // Symbols 3
-                      [128367, 128512] ]; // Symbols 4
-//                      [128681, 128759] ]; // Signs and Warnings (needs cleanup)
-
-
-//let emojiCodeListRange = 1425; // (129425-128000)
+let emojiCodeList = [ [128512, 128592], [129296, 129304], // Smileys 1+2
+                      [129312, 129343], [128100, 128132], // Smileys 3 + People 1
+                      [129304, 129311], [128064, 128100], // Gestures 1 + Gestures 2 / Fashion
+                      [128000, 128064], [129408, 129426], // Animals 1+2
+                      [127789, 127872], [129344, 129350], // Food 1+2
+                      [129351, 129375], [129375, 129392], // Food 3+4
+                      [129472, 129473], [127872, 127956], // Cheese + Hobbies
+                      [128176, 128281], [127956, 127985], // Business / Media + Places 1
+                      [128331, 128335], [128640, 128680], // Churches + Travel/Transport
+                      [128680, 128681], [127744, 127789], // Police Siren + Weather
+                      [128293, 128306], [128132, 128176], // Symbols 1+2
+                      [9904,   10161 ], [128367, 128512] ]; // Symbols 3+4
 let fullEmojiList = [];
 let nEmojisGenerated = 0; // Track numbers of emojis printed (per section)
 let nEmojisDrawn = 0
@@ -42,35 +26,40 @@ let bDarkModeEnabled = false;
       nEmojisGenerated++;
     }
   }
+
 // Display number of symbols drawn on bottom of page
 txtDisplayCounter.innerHTML = "<i>n</i> = " + nEmojisGenerated;
-
 printEmojiTableToConsole();
 
 // Print table of Emojis
   for (let nEmojisDrawn = 0; nEmojisDrawn < nEmojisGenerated;) {
     let row = $('<tr></tr>');
     for (let y = 0; y < 10; y++) {
-      let cell = $('<td id = cell-' + nEmojisDrawn + '></td>');
+      let cell = $('<td id = emoji-' + nEmojisDrawn + '></td>');
       $(cell).html(fullEmojiList[nEmojisDrawn]);
-//      $(cell).text(fullEmojiList[nEmojisDrawn]);
-      $(cell).attr('title', fullEmojiList[nEmojisDrawn]);
+      $(cell).attr('title', 'Copy to clipboard (' + fullEmojiList[nEmojisDrawn] + ')');
       $(cell).addClass('emojiTableStyle');
       $(cell).appendTo(row);
       if(nEmojisGenerated == fullEmojiList.length) {
         $(cell).html('<b><img src=images/menu.png width=50 id=btnDarkMode></b>');
         $(cell).attr('title','🌜 Click to toggle between Dark and Light Mode 🌞');
+        $(cell).attr('id', 'mainButton');
       }
       nEmojisDrawn++
       cellCount++;
       fullEmojiList[nEmojisGenerated] = "&#" + y + ";";
-//      console.log(`nEmojisDrawn = ${nEmojisDrawn}, y = ${y}`);
     }
   $(row).appendTo('table');
   }
 
-//console.log(`nEmojisGenerated = ${nEmojisGenerated}, nEmojisDrawn = ${nEmojisDrawn}, cellCount = ${cellCount}`);
-//console.log(`emojiCodeList.length = ${emojiCodeList.length}`)
+// Copy Emoji to clipboard when clicked
+$(document).ready(function() {
+  $("[id^=emoji]").click(function() {
+    let txtCopy = $(this).closest('td').html();
+    navigator.clipboard.writeText(txtCopy);
+    displayCopyToast(txtCopy);
+  });
+});
 
 // Toggle dark mode on/off
 $(document).ready(function() {
@@ -92,6 +81,16 @@ function toggleDarkMode() {
     $(btnDarkMode).attr('title','🌙 Dark Mode');
     bDarkModeEnabled = true;
   }
+}
+
+function displayCopyToast(txt) {
+  // Display notification
+  let x = document.getElementById("toastMessage");
+  x.innerHTML = "Copied " + txt + " to clipboard";
+  x.className = "show";
+  setTimeout(function() {
+    x.className = x.className.replace("show", "");
+  }, 2500);
 }
 
 // Print Emoji array with Markdown table syntax to console
