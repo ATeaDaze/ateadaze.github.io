e3a57642b0d3070d5e4bdcf97d48e792fccdd949
ATeaDaze <89423150+ATeaDaze@users.noreply.github.com>
2023-11-07 06:16:37 -0600

    Add files via upload

--- emoji_table/scripts/main.js
+++ emoji_table/scripts/main.js
@@ -45,11 +45,11 @@ function printTableData(fullList, nGenerated, bEnablePrintBtn) {
         $(cell).attr('title','🌜 Click to toggle between dark and light mode 🌞');
         $(cell).attr('id', 'menuButtonCell');
       } // Deactivate any cell without a valid emoji or flag
-/*      if( ((nCellsDrawn > 1122) && (nCellsDrawn < 1130)) || (nCellsDrawn > 1386) ) {
+      if( ((nCellsDrawn > 1272) && (nCellsDrawn < 1280)) || (nCellsDrawn > 1536) ) {
         $(cell).html('');
         $(cell).attr('id', 'null-cell-' + nCellsDrawn);
         $(cell).attr('title','');
-      }*/
+      }
       $(cell).appendTo(row);
       nDrawn++
       nCellsDrawn++;
